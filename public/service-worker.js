33f05e59daf303a3236e574831aafe034094157b
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-04 20:17:23 -0500

    service worker fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-// Caching all of the files within the/public folder
+// Caching all of the files within the public folder
 const FILES_TO_CACHE = [
   "/",
   "/icons/icon-192x192.png",
@@ -6,12 +6,14 @@ const FILES_TO_CACHE = [
   "/index.html",
   "index.js",
   "/db.js",
-  "/styles.css",
   "manifest.json",
+  "/styles.css",
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+//if ("service worker" in navigator)
 
 // Install
 self.addEventListener("install", function (evt) {
